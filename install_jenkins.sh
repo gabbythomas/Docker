cc103d54c928e52bf0a2945637a4f6775df655e8
gabby thomas <30359487+gabbythomas@users.noreply.github.com>
2021-07-09 20:21:40 -0700

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -7,6 +7,5 @@ docker run -d --rm -v jenkins_home:/var/jenkins_home \
            -v /var/run/docker.sock:/var/run/docker.sock \
            -v $(which docker):$(which docker) \
            --group-add $(stat -c '%g' /var/run/docker.sock) \
-           --env-file jenkins-env.txt \
            -p 0.0.0.0:8080:8080 -p 50000:50000 --name jenkins jenkins/jenkins
 
