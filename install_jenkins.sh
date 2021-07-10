033ec237ed19fb83d910bcd685b84815f2dcf469
Ubuntu <ubuntu@ip-172-31-36-220.us-east-2.compute.internal>
2021-07-10 03:36:20 +0000

    use dockerfile to build base image instead

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,11 +1,13 @@
 # If docker isn't setup to be run as non-root user,
 # this script must be run with sudo.
 
-docker pull jenkins/jenkins
+docker container kill jenkins
+
+docker build -f jenkins-with-build-tools.dockerfile -t jenkins .
 
 docker run -d --rm -v jenkins_home:/var/jenkins_home \
            -v /var/run/docker.sock:/var/run/docker.sock \
            -v $(which docker):$(which docker) \
            --group-add $(stat -c '%g' /var/run/docker.sock) \
-           -p 0.0.0.0:8080:8080 -p 50000:50000 --name jenkins jenkins/jenkins
+           -p 0.0.0.0:8080:8080 -p 50000:50000 --name jenkins jenkins
 
